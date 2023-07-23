87b2504efdfef28875e46f666b1981821db7e9fc
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 22:55:48 +0200

    Formatting.

--- src/main/java/de/photon/effects/Effects.java
+++ src/main/java/de/photon/effects/Effects.java
@@ -3,9 +3,11 @@ package de.photon.effects;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Effects extends JavaPlugin {
+public class Effects extends JavaPlugin
+{
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         final PluginCommand effectCommand = this.getCommand("effects");
 
         if (effectCommand == null) {
