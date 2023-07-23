aa02d2b199b37fbdc060fbe410cbb414eadf7de0
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 22:49:49 +0200

    Better error message when command could not be registered.

--- src/main/java/de/photon/effects/Effects.java
+++ src/main/java/de/photon/effects/Effects.java
@@ -1,13 +1,19 @@
 package de.photon.effects;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Effects extends JavaPlugin
-{
+public class Effects extends JavaPlugin {
     @Override
-    public void onEnable()
-    {
-        this.getCommand("effects").setExecutor(MainCommand.getInstance());
-        this.getCommand("effects").setTabCompleter(MainCommand.getInstance());
+    public void onEnable() {
+        final PluginCommand effectCommand = this.getCommand("effects");
+
+        if (effectCommand == null) {
+            this.getLogger().severe("Could not register /effects command. Disabling plugin.");
+            return;
+        }
+
+        effectCommand.setExecutor(MainCommand.getInstance());
+        effectCommand.setTabCompleter(MainCommand.getInstance());
     }
 }
